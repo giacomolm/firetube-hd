af9a8f8e2c3194289f43be8e9e0437c61a585deb
giacomolm <giacomolm@hotmail.it>
2017-02-23 22:46:56 +0100

    Still alive, at least link copy-paste and change format

--- js/views/playerView.js
+++ js/views/playerView.js
@@ -1,4 +1,4 @@
-define(["jquery", "underscore", "backbone", "ractive", "collections/Videos","utils","views/searchView","views/searchDLview","text!templates/playerView.html"],
+define(["jquery", "underscore", "backbone", "ractive", "collections/Videos","utils","views/searchView","views/searchDLView","text!templates/playerView.html"],
     function ($, _, Backbone, Ractive, Videos, Utils, searchView, searchDLView,template) {
 
     var playerView = Backbone.View.extend({
@@ -7,9 +7,9 @@ define(["jquery", "underscore", "backbone", "ractive", "collections/Videos","uti
             "change #formats" : "changeFormat",
         },
 	
-        initialize: function () {
-		if(this.model) this.setModel(this.model);
-        },
+    initialize: function () {
+		  if(this.model) this.setModel(this.model);
+    },
 	
 	setModel: function(model){
 		//i'm creating an empty collection in order to fill the right model, throught the display method
@@ -36,12 +36,12 @@ define(["jquery", "underscore", "backbone", "ractive", "collections/Videos","uti
 	},
 	
 	setVideos: function(){
-		this.data = this.collection.get(0);	
+		this.data = this.collection;
 		this.render();
 	},
 	
 	setDLVideos: function(){
-		this.data = this.collection.get(0);
+		this.data = this.collection;
 		this.render();
 	},
        
@@ -49,20 +49,18 @@ define(["jquery", "underscore", "backbone", "ractive", "collections/Videos","uti
 		var id = (ev.target).value;
 		//return to top once changed
 		window.scrollTo(0,0);
-		Utils.getVideoPlayer().src = this.data.urls[id].url;
+		Utils.getVideoPlayer().src = this.data.models[id].url;
 	},
 
-        render: function (eventName) {
-	    
-	    //IMPORTANT: i'm not appending the video info to the current div, but under the video	
-            this.template = new Ractive({el : $('#player'), template: template, data: this.data});
-	    
-	    if(this.relatedView) $("#container").html(this.relatedView.el);
-            return this;
-        }
-       
-      });
+    render: function (eventName) {
+      //IMPORTANT: i'm not appending the video info to the current div, but under the video
+      this.template = new Ractive({el : $('#player'), template: template, data: this.data});
 
-    return playerView;
+	    if(this.relatedView) $("#container").html(this.relatedView.el); return this;
+    }
 
   });
+
+  return playerView;
+
+});
